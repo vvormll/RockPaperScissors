bccd00fa413f169589320a8acebfda054ae2d7c8
vvormll <vvormll@gmail.com>
2018-08-22 19:19:37 +0300

    Add Espresso Add tests for MainActivity and PickGameModeActivity

---
+++ app/src/androidTest/java/my/projects/rockpaperscissors/MainActivityEspressoTest.java
@@ -0,0 +1,39 @@
+package my.projects.rockpaperscissors;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityEspressoTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mainActivityActivityTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void showsStartGameButtonOnStart() {
+        onView(withId(R.id.startButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnStartGameButtonStartsPickModeActivity() {
+        onView(withId(R.id.startButton)).perform(click());
+
+        intended(hasComponent(hasShortClassName(".PickGameModeActivity")));
+    }
+}
---
+++ app/src/androidTest/java/my/projects/rockpaperscissors/PickGameModeActivityEspressoTest.java
@@ -0,0 +1,46 @@
+package my.projects.rockpaperscissors;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class PickGameModeActivityEspressoTest {
+
+    @Rule
+    public IntentsTestRule<PickGameModeActivity> activityIntentsTestRule = new IntentsTestRule<>(PickGameModeActivity.class);
+
+    @Test
+    public void showsGameModeButtons() {
+        onView(withId(R.id.rpsButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.rpslsButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickOnRPSButtonSendIntentWithRPSGameMode() {
+        onView(withId(R.id.rpsButton)).perform(click());
+
+        intended(allOf(hasComponent(hasShortClassName(".GameActivity")), hasExtra(PickGameModeActivity.GAME_MODE_KEY, GameMode.ROCK_PAPER_SCISSORS)));
+    }
+
+    @Test
+    public void clickOnRPSLSButtonSendIntentWithRPSLSGameMode() {
+        onView(withId(R.id.rpslsButton)).perform(click());
+
+        intended(allOf(hasComponent(hasShortClassName(".GameActivity")), hasExtra(PickGameModeActivity.GAME_MODE_KEY, GameMode.ROCK_PAPER_SCISSORS_LIZARD_SPOCK)));
+    }
+}
